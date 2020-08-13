47daa1903e5aa12d55b60e9d1e54120b2171bd77
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-13 02:04:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def base_hash
-  railroads = {
+  railroads={
     :railroads=> {}
   }
   railroads
 end
 
 def monopoly_with_second_tier
-  :railroads= [ 
+  railroads = [ 
     {:pieces=>4},
     {:rent_in_dollars=> {}}, 
     {:name=> {}}
